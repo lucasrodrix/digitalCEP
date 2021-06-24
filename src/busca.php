0c860123efe7b6fb816b8596ce867bbd796f8e28
Lucas Rodrix <lucasrodrigues1985.lfr@gmail.com>
2021-06-24 12:31:17 -0300

    gerenciando pacotes e Criando código utilizando o Composer

---
+++ src/busca.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once "vendor/autoload.php";
+
+use \Lucas\DigitalCep\Search;
+
+$busca = new Search;
+$resultado = $busca->getAddressFromZipCode('09852820');
+
+print_r($resultado);
